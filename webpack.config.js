471c7a5d187c7321aa021c7e2e042ae0255591b4
KyoungDuckKim <skate2@naver.com>
2020-06-30 17:29:43 +0900

    change project settings(dependency, gitignore, webpack)

--- webpack.config.js
+++ webpack.config.js
@@ -8,82 +8,82 @@ const nodeExternals = require('webpack-node-externals');
 require('dotenv').config();
 
 module.exports = (env, argv) => {
-    let isProductionMode = (env && env.production) || process.env.production === 'true' || false;
-    let devtool;
-    const plugins = [new MiniCssExtractPlugin({ filename: 'app.css' }), new NodemonPlugin()];
+	let isProductionMode = (env && env.production) || process.env.production === 'true' || false;
+	let devtool;
+	const plugins = [new MiniCssExtractPlugin({ filename: 'app.css' }), new NodemonPlugin()];
 
-    if (isProductionMode) {
-        isProductionMode = 'production';
-        devtool = false;
-        plugins.push(new CleanWebpackPlugin());
-    } else {
-        isProductionMode = 'development';
-        devtool = 'inline-source-map';
-    }
+	if (isProductionMode) {
+		isProductionMode = 'production';
+		devtool = false;
+		plugins.push(new CleanWebpackPlugin());
+	} else {
+		isProductionMode = 'development';
+		devtool = 'inline-source-map';
+	}
 
-    return {
-        target: 'node',
-        mode: isProductionMode,
-        devtool,
-        entry: './src/index',
-        output: {
-            path: path.resolve(__dirname, 'build'),
-            filename: '[name].js',
-            sourceMapFilename: '[file].map',
-            publicPath: '/',
-        },
-        devServer: {
-            hot: true,
-            host: 'localhost',
-            port: 3000,
-            contentBase: path.resolve(__dirname, 'build'),
-        },
-        resolve: {
-            modules: [path.join(__dirname, 'src'), 'node_modules'],
-            extensions: ['.ts', '.tsx', '.js', '.json'],
-            plugins: [new TsconfigPathsPlugin()],
-        },
-        module: {
-            rules: [
-                {
-                    // Include ts, tsx, js, and jsx files.
-                    test: /\.(ts|js)x?$/,
-                    exclude: /node_modules/,
-                    loader: 'ts-loader',
-                },
-                {
-                    // Images
-                    test: /\.(png|svg|jpe?g|gif)$/,
-                    loader: 'url-loader',
-                    options: {
-                        outputPath: 'images/',
-                        limit: 10000,
-                    },
-                },
-                {
-                    // Font files.
-                    test: /\.(woff|woff2|eot|ttf|otf)$/,
-                    loader: 'file-loader',
-                },
-                {
-                    // css, scss files.
-                    test: /\.css$/,
-                    exclude: /node_modules/,
-                    use: [MiniCssExtractPlugin.loader, 'css-loader'],
-                },
-                {
-                    exclude: /node_modules/,
-                    test: /\.graphql$/,
-                    use: [{ loader: 'graphql-import-loader' }],
-                },
-            ],
-        },
-        optimization: {
-            splitChunks: {
-                chunks: 'all',
-            },
-        },
-        externals: [nodeExternals()],
-        plugins,
-    };
+	return {
+		target: 'node',
+		mode: isProductionMode,
+		devtool,
+		entry: './src/index',
+		output: {
+			path: path.resolve(__dirname, 'build'),
+			filename: '[name].js',
+			sourceMapFilename: '[file].map',
+			publicPath: '/',
+		},
+		devServer: {
+			hot: true,
+			host: 'localhost',
+			port: 3000,
+			contentBase: path.resolve(__dirname, 'build'),
+		},
+		resolve: {
+			modules: [path.join(__dirname, 'src'), 'node_modules'],
+			extensions: ['.ts', '.tsx', '.js', '.json'],
+			plugins: [new TsconfigPathsPlugin()],
+		},
+		module: {
+			rules: [
+				{
+					// Include ts, tsx, js, and jsx files.
+					test: /\.(ts|js)x?$/,
+					exclude: /node_modules/,
+					loader: 'ts-loader',
+				},
+				{
+					// Images
+					test: /\.(png|svg|jpe?g|gif)$/,
+					loader: 'url-loader',
+					options: {
+						outputPath: 'images/',
+						limit: 10000,
+					},
+				},
+				{
+					// Font files.
+					test: /\.(woff|woff2|eot|ttf|otf)$/,
+					loader: 'file-loader',
+				},
+				{
+					// css, scss files.
+					test: /\.css$/,
+					exclude: /node_modules/,
+					use: [MiniCssExtractPlugin.loader, 'css-loader'],
+				},
+				{
+					exclude: /node_modules/,
+					test: /\.graphql$/,
+					use: [{ loader: 'graphql-import-loader' }],
+				},
+			],
+		},
+		optimization: {
+			splitChunks: {
+				chunks: 'all',
+			},
+		},
+		externals: [nodeExternals()],
+		plugins,
+	};
 };
