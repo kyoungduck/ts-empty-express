3560c5cee078b809ddd48cfa242d1ccd6ad6f165
KyoungDuckKim <skate2@naver.com>
2020-06-30 18:06:48 +0900

    Change the log message displayed when 'express' starts

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ const PORT = process.env.SERVER_PORT || 3000;
 const packageVersion = require('../package.json').version;
 
 app.listen({ port: PORT }, () => {
-	console.log(`vipho upload server running port : ${PORT}`);
-	console.log(`upload server version ${packageVersion}`);
+	console.log(`service running port : ${PORT}`);
+	console.log(`service version ${packageVersion}`);
 });
