eec451afbad07c694d8aace027bcaf6ef523a920
KyoungDuckKim <skate2@naver.com>
2020-10-18 03:06:12 +0900

    refact: webpack refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,14 @@
 const path = require('path');
 const webpack = require('webpack');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const NodemonPlugin = require('nodemon-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const NodemonPlugin = require('nodemon-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
-require('dotenv').config();
 
 module.exports = (env, argv) => {
 	let isProductionMode = (env && env.production) || process.env.production === 'true' || false;
 	let devtool;
-	const plugins = [new MiniCssExtractPlugin({ filename: 'app.css' }), new NodemonPlugin()];
+	const plugins = [new NodemonPlugin()];
 
 	if (isProductionMode) {
 		isProductionMode = 'production';
@@ -32,15 +30,9 @@ module.exports = (env, argv) => {
 			sourceMapFilename: '[file].map',
 			publicPath: '/',
 		},
-		devServer: {
-			hot: true,
-			host: 'localhost',
-			port: 3000,
-			contentBase: path.resolve(__dirname, 'build'),
-		},
 		resolve: {
 			modules: [path.join(__dirname, 'src'), 'node_modules'],
-			extensions: ['.ts', '.tsx', '.js', '.json'],
+			extensions: ['.ts', '.js', '.json'],
 			plugins: [new TsconfigPathsPlugin()],
 		},
 		module: {
@@ -50,31 +42,6 @@ module.exports = (env, argv) => {
 					test: /\.(ts|js)x?$/,
 					exclude: /node_modules/,
 					loader: 'ts-loader',
-				},
-				{
-					// Images
-					test: /\.(png|svg|jpe?g|gif)$/,
-					loader: 'url-loader',
-					options: {
-						outputPath: 'images/',
-						limit: 10000,
-					},
-				},
-				{
-					// Font files.
-					test: /\.(woff|woff2|eot|ttf|otf)$/,
-					loader: 'file-loader',
-				},
-				{
-					// css, scss files.
-					test: /\.css$/,
-					exclude: /node_modules/,
-					use: [MiniCssExtractPlugin.loader, 'css-loader'],
-				},
-				{
-					exclude: /node_modules/,
-					test: /\.graphql$/,
-					use: [{ loader: 'graphql-import-loader' }],
 				},
 			],
 		},
